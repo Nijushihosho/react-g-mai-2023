3d5b21719d94ab2c532f3c1a9e904e4f2a6e2f69
Malik H <malik.h@webdevpro.net>
2023-05-24 11:22:54 +0200

    cas pratique

---
+++ jour1-decouverte/src/composants/EventSuite.jsx
@@ -0,0 +1,19 @@
+function EventSuite(){
+
+    const fleurs = [
+        { id : 1 , nom : "rose" , prix : 20 },
+        { id : 2 , nom : "jasmin" , prix : 120 },
+        { id : 3 , nom : "tulipe" , prix : 30 },
+    ];
+
+    // dans la vue afficher pour chaque fleur 
+    // <div>
+    // <p> rose <button>prix</button> </p> => si je clique => alert(20)
+    // <p> jasmin <button>prix</button> </p> => si je clique => alert(120)
+    // <p> tulipe <button>prix</button> </p> => si je clique => alert(30)
+    // </div>
+    // cas pratique : réaliser le jsx ci dessus 
+
+}
+
+export default EventSuite ;
