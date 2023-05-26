468cc350839c9e09a323548af6603f2d482c8fa2
Malik H <malik.h@webdevpro.net>
2023-05-26 10:37:25 +0200

    props suite

--- jour4-communication/src/composants/Compteur.jsx
+++ jour4-communication/src/composants/Compteur.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 
 function Compteur({nb, couleur}) {
+
   return (
     <div>
         Compteur {nb} 
-        <button style={{ background : couleur }}>augmenter</button>
+        <button style={ { background : couleur }}>augmenter</button>
         {/* <pre>{JSON.stringify(props , null , "   ")}</pre> */}
     </div>
   )
--- jour4-communication/src/composants/Sommaire.jsx
+++ jour4-communication/src/composants/Sommaire.jsx
@@ -3,10 +3,29 @@ import React from 'react'
 import Compteur from './Compteur'
 
 function Sommaire() {
+    const compteurs = [
+        { nb : 10 ,couleur : '#FF0000'},
+        { nb : 0 ,couleur : '#2ecc71'},
+        { nb : -3 ,couleur : '#9b59b6'},
+        { nb : -3 ,couleur : '#9b59b6'},
+        { nb : -3 ,couleur : '#9b59b6'},
+        { nb : -3 ,couleur : '#9b59b6'},
+        { nb : -3 ,couleur : '#9b59b6'},
+        { nb : 22 ,couleur : '#00ee00'},
+        { nb : -3 ,couleur : '#9b59b6'},
+        { nb : 44 ,couleur : '#0000FF'},
+        { nb : -3 ,couleur : '#9b59b6'},
+        { nb : -3 ,couleur : '#9b59b6'},
+        { nb : -3 ,couleur : '#9b59b6'},
+    ]
+
   return (
     <div>
         <h2>Sommaire</h2>
-        <Compteur nb={10} couleur={'#FF0000'} />
+        {compteurs.map(function({nb, couleur} , key){
+            return  <Compteur nb={nb} couleur={couleur} key={key} />
+        })}
+        
         {/** nb est une props (rappel les attributs d'une balise html)
          * vous êtes libre de l'appeler comme vous voulez 
          * https://colorhunt.co/
@@ -14,8 +33,8 @@ function Sommaire() {
          * https://flatuicolors.com/
          * https://color.adobe.com
            */}
-        <Compteur nb={ 0 } couleur={'#2ecc71'}/>
-        <Compteur nb={ -3 } couleur={'#9b59b6'}/>
+       {/*  <Compteur nb={ 0 } couleur={'#2ecc71'}/>
+        <Compteur nb={ -3 } couleur={'#9b59b6'}/> */}
     </div>
   )
 }
