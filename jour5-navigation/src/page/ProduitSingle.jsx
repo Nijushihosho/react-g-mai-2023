74650a9ec68305541db4ba130efe845042e9b7ff
Malik H <malik.h@webdevpro.net>
2023-06-06 14:27:45 +0200

    LightBox

--- jour5-navigation/src/page/ProduitSingle.jsx
+++ jour5-navigation/src/page/ProduitSingle.jsx
@@ -1,7 +1,10 @@
 import React , {useEffect, useState} from 'react'
-import { useParams } from "react-router-dom"
+import { useParams , Link } from "react-router-dom"
+import "./ProduitSingle.css"; 
+
 function ProduitSingle() {
-    const {id, title} = useParams() ; // permet de récupérer la partie variable dans l'url :id/:title définis dans le router 
+    const {id} = useParams() ; // permet de récupérer la partie variable dans l'url :id/:title définis dans le router 
+    
     const [produit, setProduit] = useState({});
     useEffect( function(){ // useEffect() => React => hook 
         // permet de remplir la variable produit via une requete ajax au moment du chargement du composant dans le DOM 
@@ -9,18 +12,34 @@ function ProduitSingle() {
             .then(function(reponse){ return reponse.json()})
             .then(function( data ){ setProduit(data)})
     } , [])
+
+    const [lightBox, setLightBox] = useState( { show : false , img : ""} )
+
+    function showLightBox(url){
+        setLightBox({ show : true , img : url })
+    }
+    function hideLightBox(){
+        setLightBox({ show : false , img : "" })
+    }
   return (
     <div>
+        { lightBox.show && <div className='light-box' onClick={hideLightBox}>
+            <button >fermer</button>
+            <img src={lightBox.img} alt="" />
+        </div>  }
     { Object.keys(produit).length > 0 && <div className='container'>
     <section className="row mt-4">
         <figure className="col-4">
-            <img src={produit.thumbnail} alt="" className='img-fluid mb-3' />
-            <div className="row">
+            <img src={produit.thumbnail} alt="" className='img-fluid mb-3' onClick={ function(){ showLightBox(produit.thumbnail) }} />
+            <div className="row" >
                 {produit.images && produit.images.map(function(item, key){
                     return <div className='col-3'  key={key}>
-                        <img src={item} alt=""  className='mw-100 border border-primary' />
+                        <img src={item} alt=""  className='mw-100 border border-primary' onClick={ function(){ showLightBox(item) }} />
                     </div>
                 }) }
+            </div>
+            <div className='text-center mt-3'>
+                <Link to="/produits" className='btn btn-danger btn-sm'>Retour aux produits</Link>
             </div>
         </figure>
         <div className="col-8">
