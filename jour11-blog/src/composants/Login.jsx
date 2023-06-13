5d9a5ba5d2a7e581c462c4472831360a02f3118a
Malik H <malik.h@webdevpro.net>
2023-06-13 14:10:12 +0200

    authentification via gmail

--- jour11-blog/src/composants/Login.jsx
+++ jour11-blog/src/composants/Login.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
 
+import { signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+import { auth, provider } from '../config/firebase'; 
+
+
 function Login() {
+
+    const googleHandler = async () => {
+        provider.setCustomParameters({ prompt: 'select_account' });
+        signInWithPopup(auth, provider)
+            .then((result) => {
+                // This gives you a Google Access Token. You can use it to access the Google API.
+                const credential = GoogleAuthProvider.credentialFromResult(result);
+                const token = credential.accessToken;
+                // The signed-in user info.
+                const user = result.user;
+                // redux action? --> dispatch({ type: SET_USER, user });
+            })
+            .catch((error) => {
+                // Handle Errors here.
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                // The email of the user's account used.
+                const email = error.email;
+                // The AuthCredential type that was used.
+                const credential = GoogleAuthProvider.credentialFromError(error);
+                // ...
+            });
+    };
+
   return (
-    <div>Login</div>
+    <div>
+        <button onClick={googleHandler}>authentification via votre compte GMail</button>
+    </div>
   )
 }
 
---
+++ jour11-blog/src/config/firebase.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth , GoogleAuthProvider } from "firebase/auth"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBamhsG7lo_vjESVJoot_ToSAUB8gAcoLs",
+  authDomain: "fir-greta.firebaseapp.com",
+  projectId: "fir-greta",
+  storageBucket: "fir-greta.appspot.com",
+  messagingSenderId: "620538885768",
+  appId: "1:620538885768:web:b539dba4804b8676d5113d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app); 
+export const auth = getAuth();
+export const provider = new GoogleAuthProvider(); 
