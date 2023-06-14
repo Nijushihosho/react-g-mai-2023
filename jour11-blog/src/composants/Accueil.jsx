391cbfef6e8babf2f3a3f660fdcc5a6370808151
Malik H <malik.h@webdevpro.net>
2023-06-14 11:47:17 +0200

    correction

--- jour11-blog/src/composants/Accueil.jsx
+++ jour11-blog/src/composants/Accueil.jsx
@@ -2,12 +2,18 @@ import React , {useContext , useEffect, useState} from 'react'
 import { AuthContext } from '../context/AuthContext'
 import  Form  from './Form';
 import {db} from "../config/firebase"
-import { collection, getDocs , doc , deleteDoc } from "firebase/firestore";
+import { collection, getDocs , doc , deleteDoc , updateDoc  } from "firebase/firestore";
 import mario from "../assets/mario.jpg"
 
 
 function Accueil({nom}) {
   const {isLogged} = useContext(AuthContext);
+
+  const [articles, setArticles] = useState([])
+  const [update, setUpdate] = useState(true)
+  const [id , setId] = useState(0)
+  const [localArticle, setLocalArticle]= useState({})
+
   const getNotes = async () => {
       const notesSnapshot = await getDocs(collection(db, "articles"));
       const notesList = notesSnapshot.docs.map((doc) => {
@@ -16,16 +22,35 @@ function Accueil({nom}) {
       setArticles(notesList);
   };
 
-  const [articles, setArticles] = useState([])
-  const [update, setUpdate] = useState(true)
   useEffect( function(){
     getNotes()
   }, [update])
+  
+  function modifier(id , article){
+    setId(id)
+    setLocalArticle(article)
+  }
 
   async function supprimer(id){
     const ref = doc(db, "articles", id)
     await deleteDoc(ref)
     setUpdate(!update);
+  }
+  function change(e){
+    const cloneLocalArticle = structuredClone(localArticle)
+    cloneLocalArticle[e.target.name] = e.target.value;
+    setLocalArticle(cloneLocalArticle); 
+  }
+  async function submit(e){
+    e.preventDefault();
+    if(localArticle.titre.length < 3 || localArticle.body.length < 3){
+      alert("attention formulaire invalide")
+      return ;
+    }
+    const ref = doc(db, "articles", localArticle.id);
+    await updateDoc(ref, localArticle);
+    setUpdate(!update);
+    setId(0);
   }
 
   return (
@@ -40,13 +65,23 @@ function Accueil({nom}) {
       <main>
         { articles.map(function(item, key){
           return <article key={key}>
-            <h2>{item.titre}</h2>
-            <p>{item.body}</p>
-            <p>{item?.dt_creation && "Publié le : "} {item?.dt_creation && new Intl.DateTimeFormat("fr-FR").format(new Date(item?.dt_creation?.seconds *1000))}</p>
-            {isLogged() && <>
-              <button className='me-3'>update</button>
-              <button onClick={function(){ supprimer(item.id)  }}>delete</button>
-            </>}
+            { id === item.id ? <form className='form-add' onSubmit={submit}>
+                <input type="hidden" name="id" value={localArticle.id} />
+                <input type="text" value={localArticle.titre} name="titre" onChange={function(e){ change(e) }}/>
+                <textarea value={localArticle.body} name='body' rows={5} onChange={function(e){ change(e) }}></textarea>
+                <input type='submit' value="mettre à jour" />
+            </form> :
+              <>
+                <h2>{item.titre}</h2>
+                  <p>{item.body}</p>
+                  <p>{item?.dt_creation && "Publié le : "} {item?.dt_creation && new Intl.DateTimeFormat("fr-FR").format(new Date(item?.dt_creation?.seconds *1000))}</p>
+                  {isLogged() && <>
+                    <button className='me-3' onClick={ function(){ modifier(item.id , item) }  }>update</button>
+                    <button onClick={function(){ supprimer(item.id)  }}>delete</button>
+                  </>}
+              </>
+            }
+            
           </article>
         }) }
       </main>
